eb57a95798e684cd95b853ad0242bc104883714f
Naol Melesse <naolmelesse91@gmail.com>
2023-09-30 20:33:51 +0300

    title update in gameslist

--- app/components/games.tsx
+++ app/components/games.tsx
@@ -35,7 +35,7 @@ export default async function GameList() {
 
   return (
     <div>
-      <h1 className="text-3xl font-semibold mb-4">Top Games</h1>
+      <h1 className="text-3xl font-semibold mb-4">Top Games for Milky</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {games.map((game) => 
           <Card key={game.id}>
