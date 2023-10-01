192cf54e139723ccd16aa3df92101c2f6538a180
Naol Melesse <naolmelesse91@gmail.com>
2023-10-01 11:16:03 +0300

    another type error

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -1,10 +1,10 @@
-'use client'
+
 import { useSearchParams } from 'next/navigation'
 import { AiFillStar } from "react-icons/ai"
 import Image from 'next/image';
 type Game = {
     name: string,
-    description: string,
+    description_raw: string,
     background_image_additional: string,
     rating: number,
     id: number,
@@ -15,7 +15,7 @@ type Game = {
         data: { }
         },
 }
-const getGame = async (id : number) : Promise<Game> => {
+const getGame = async (id : string | null) : Promise<Game> => {
         const apiKey = process.env.NEXT_PUBLIC_RAWG_API_KEY;
         const response = await fetch(`https://api.rawg.io/api/games/${id}?key=87d59138f1444b409dad7a688c402e4e`);
         const moviesRes = await fetch(`https://api.rawg.io/api/games/${id}/movies?key=87d59138f1444b409dad7a688c402e4e`);
@@ -25,26 +25,27 @@ const getGame = async (id : number) : Promise<Game> => {
         return data
 }
 
-export default async function Game(){
-    const searchParams = useSearchParams();
-    const id = searchParams.get('id');
+export default async function Game({params } : any){
+    const game = await getGame(params.id);
 
-    const game = await getGame(Number(id));
     return(
-        <div className="flex flex-col lg:flex-row justify-between items-center">
-            <div className="aspect-video relative w-3/5">
-            <Image
-                src={game.background_image_additional}
-                alt={game.name}
-                fill
-                className="object-cover"
-                />
+        <div>
+            <div className="flex flex-col lg:flex-row mx-auto justify-between items-center">
+                <div className="aspect-video relative w-2/5">
+                    <Image
+                        src={game.background_image_additional}
+                        alt={game.name}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+                <div className="w-1/3">
+                    <h1>{game.name}</h1>
+                    <p className="text-gray-600 mb-4 flex gap-1 items-center"><AiFillStar/> {game.rating}</p>
+            
+                </div> 
             </div>
-            <div className="w-1/3">
-                <h1>{game.name}</h1>
-                <p className="text-gray-600 mb-4 flex gap-1 items-center"><AiFillStar/> {game.rating}</p>
-                {game.description}
-            </div>
+            <p>{game.description_raw}</p> 
         </div>
     )
 }
